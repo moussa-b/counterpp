b250d5df2e633c7780a4b98970f20fad59755a0e
moussa <moussa@bdmz.fr>
2025-07-28 10:07:15 +0200

    update google playstore share url

--- lib/models/app_config.dart
+++ lib/models/app_config.dart
@@ -1,5 +1,5 @@
-class AppConfig { // TODO complete with real values
-  static const String shareUrl = 'https://example.com';
+class AppConfig {
+  static const String shareUrl = 'https://play.google.com/store/apps/details?id=com.bdzapps.counter.counter';
   static const String developerEmail = 'boudamouzm@gmail.com';
   static const String appName = 'Counter++';
 }
